8796e9566726da111e3147d86ad3ead414fdec1e
Arlopess <arnaldolopesassuncao@gmail.com>
2026-09-20 15:27:51 -0300

    ano.js

---
+++ ano.js
@@ -0,0 +1,6 @@
+let ano = Number(prompt(`${nome}, digite o ano do seu nascimento: `))
+
+if (2026 - ano) {
+    alert(`voce tem ${2026 - ano} anos`)
+    console.log(`voce tem ${2026 - ano} anos`)
+}
