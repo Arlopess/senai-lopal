5a2c1065c6c5f0a2fff857256c28e71fbb13f2c9
Arlopess <arnaldolopesassuncao@gmail.com>
2026-09-20 00:34:13 -0300

    exercice

---
+++ idade.js
@@ -0,0 +1,9 @@
+let idade = Number(prompt(`${nome}, digite sua idade: `))
+
+if (idade >= 18) {
+    alert(`${nome}, você é maior de idade!`)
+    console.log(`${nome}, você é maior de idade!`)
+}else {
+    alert(`${nome}, você é menor de idade!`)
+    console.log(`${nome}, você é menor de idade!`)
+}
