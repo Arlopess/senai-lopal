8f78f540d9618f6237015c461e4184423f01d9aa
Arlopess <arnaldolopesassuncao@gmail.com>
2026-09-20 00:35:19 -0300

    nots.js

---
+++ nota.js
@@ -0,0 +1,9 @@
+let nota = Number(prompt(`${nome}, digite sua nota: `))
+
+if (nota >= 7) {
+    alert(`${nome}, você foi aprovado!`)
+    console.log(`${nome}, você foi aprovado!`)
+} else {
+    alert(`${nome}, você foi reprovado!`)
+    console.log(`${nome}, você foi reprovado!`)
+}
