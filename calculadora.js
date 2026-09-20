594331b218ebe81c99d7988b608a74aa789d6353
Arlopess <arnaldolopesassuncao@gmail.com>
2026-09-20 00:33:43 -0300

    activide

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+let num_one = Number(prompt (`${nome} digite o primeiro número:`))
+ let num_two = Number(prompt (`${nome} digite o segundo número:`))
+
+    console.log (typeof num_one);
+    console.log (typeof num_two);
+
+let sum = num_one + num_two
+
+    alert(`soma: ${sum}`)
+    console.log("soma",sum)
+        
+let multiplication = num_one * num_two
+        
+    alert(`multiplicação: ${multiplication}`)
+    console.log("multiplicação",multiplication)
+
+let division = num_one / num_two
+        
+    alert(`divisão: ${division}`)
+    console.log("divisão",division)
+        
+let subtraction = num_one - num_two
+
+    alert(`subtração: ${subtraction}`)
+    console.log("subtração",subtraction)
